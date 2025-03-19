a4c53225e988e71b2266ed0f3415018976a212ca
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-03-19 20:00:22 +0300

    edited

--- src/data/ProductData.jsx
+++ src/data/ProductData.jsx
@@ -1,13 +1,23 @@
+import urun1 from "src/assets/urun1.png"
+import urun2 from "src/assets/urun2.png"
+import urun3 from "src/assets/urun3.png"
+import urun4 from "src/assets/urun4.png"
+import urun5 from "src/assets/urun5.png"
+import urun6 from "src/assets/urun6.png"
+import urun7 from "src/assets/urun7.png"
+import urun8 from "src/assets/urun8.png"
+import urun9 from "src/assets/urun9.png"
+
 const productsData = [
-    { id: 1, image: "src/assets/urun1.png", title: "Soketler", description: "Ferrules" },
-    { id: 2, image: "src/assets/urun2.png", title: "Düz Metrik Somunlu", description: "Metric Straight Fittings" },
-    { id: 3, image: "src/assets/urun3.png", title: "45° Metrik Somunlu", description: "Metric 45° Elbow Fittings" },
-    { id: 4, image: "src/assets/urun4.png", title: "90° Metrik Somunlu", description: "Metric 90° Elbow Fittings" },
-    { id: 5, image: "src/assets/urun5.png", title: "Metrik Erkek Rakorlar", description: "Metric Male Fittings" },
-    { id: 6, image: "src/assets/urun6.png", title: "UNF Düz Rakorlar", description: "UNF Straight Swivel Fittings" },
-    { id: 7, image: "src/assets/urun7.png", title: "UNF 45° Rakorlar", description: "UNF 45° Elbow Swivel Fittings" },
-    { id: 8, image: "src/assets/urun8.png", title: "UNF 90° Rakorlar", description: "UNF 90° Elbow Swivel Fittings" },
-    { id: 9, image: "src/assets/urun9.png", title: "UNF Erkek Rakorlar", description: "UNF Male Fittings" },
+    { id: 1, image: { urun1 }, title: "Soketler", description: "Ferrules" },
+    { id: 2, image: { urun2 }, title: "Düz Metrik Somunlu", description: "Metric Straight Fittings" },
+    { id: 3, image: { urun3 }, title: "45° Metrik Somunlu", description: "Metric 45° Elbow Fittings" },
+    { id: 4, image: { urun4 }, title: "90° Metrik Somunlu", description: "Metric 90° Elbow Fittings" },
+    { id: 5, image: { urun5 }, title: "Metrik Erkek Rakorlar", description: "Metric Male Fittings" },
+    { id: 6, image: { urun6 }, title: "UNF Düz Rakorlar", description: "UNF Straight Swivel Fittings" },
+    { id: 7, image: { urun7 }, title: "UNF 45° Rakorlar", description: "UNF 45° Elbow Swivel Fittings" },
+    { id: 8, image: { urun8 }, title: "UNF 90° Rakorlar", description: "UNF 90° Elbow Swivel Fittings" },
+    { id: 9, image: { urun9 }, title: "UNF Erkek Rakorlar", description: "UNF Male Fittings" },
     { id: 10, image: "src/assets/product10.jpg", title: "Ürün 10" },
     { id: 11, image: "src/assets/product11.jpg", title: "Ürün 11" },
     { id: 12, image: "src/assets/product12.jpg", title: "Ürün 12" },
