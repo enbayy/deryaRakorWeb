0da16645ab00301e45b994f3fb1c6e139f9bdc21
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-03-19 20:08:01 +0300

    edited

--- src/data/ProductData.jsx
+++ src/data/ProductData.jsx
@@ -18,17 +18,6 @@ const productsData = [
     { id: 7, image: urun7, title: "UNF 45° Rakorlar", description: "UNF 45° Elbow Swivel Fittings" },
     { id: 8, image: urun8, title: "UNF 90° Rakorlar", description: "UNF 90° Elbow Swivel Fittings" },
     { id: 9, image: urun9, title: "UNF Erkek Rakorlar", description: "UNF Male Fittings" },
-    { id: 10, image: "src/assets/product10.jpg", title: "Ürün 10" },
-    { id: 11, image: "src/assets/product11.jpg", title: "Ürün 11" },
-    { id: 12, image: "src/assets/product12.jpg", title: "Ürün 12" },
-    { id: 13, image: "src/assets/product13.jpg", title: "Ürün 13" },
-    { id: 14, image: "src/assets/product14.jpg", title: "Ürün 14" },
-    { id: 15, image: "src/assets/product15.jpg", title: "Ürün 15" },
-    { id: 16, image: "src/assets/product16.jpg", title: "Ürün 16" },
-    { id: 17, image: "src/assets/product17.jpg", title: "Ürün 17" },
-    { id: 18, image: "src/assets/product18.jpg", title: "Ürün 18" },
-    { id: 19, image: "src/assets/product19.jpg", title: "Ürün 19" },
-    { id: 20, image: "src/assets/product20.jpg", title: "Ürün 20" }
 ];
 
 export default productsData;
