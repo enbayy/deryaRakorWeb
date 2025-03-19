caaa501c4dbecbdda997153aea0057e83e19bed3
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-03-19 19:58:23 +0300

    edited

--- src/components/BrandLinks.jsx
+++ src/components/BrandLinks.jsx
@@ -15,18 +15,10 @@ const BrandLinks = () => {
 const ClipPathLinks = () => {
     return (
         <div className="grid grid-cols-4 divide-neutral-900">
-            <a href="/" target="_blank" rel="noopener noreferrer">
-                <LinkBox Icon={SiFacebook} />
-            </a>
-            <a href="/" target="_blank" rel="noopener noreferrer">
-                <LinkBox Icon={SiLinkedin} />
-            </a>
-            <a href="/" target="_blank" rel="noopener noreferrer">
-                <LinkBox Icon={SiInstagram} />
-            </a>
-            <a href="/" target="_blank" rel="noopener noreferrer">
-                <LinkBox Icon={SiTwitter} />
-            </a>
+            <LinkBox Icon={SiFacebook} href="https://facebook.com" />
+            <LinkBox Icon={SiLinkedin} href="https://linkedin.com" />
+            <LinkBox Icon={SiInstagram} href="https://instagram.com" />
+            <LinkBox Icon={SiTwitter} href="https://twitter.com" />
         </div>
     );
 };
@@ -74,17 +66,18 @@ const LinkBox = ({ Icon, href }) => {
     };
 
     return (
-        <a
-            href={href}
+        <div
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
             className="relative grid h-12 w-full place-content-center sm:h-12 md:h-12 p-2"
         >
-            <Icon className="text-xl sm:text-3xl lg:text-4xl" />
+            <a href={href} target="_blank" rel="noopener noreferrer">
+                <Icon className="text-xl sm:text-3xl lg:text-4xl" />
+            </a>
             <div ref={scope} style={{ clipPath: BOTTOM_RIGHT_CLIP }} className="absolute inset-0 grid place-content-center bg-[#D22128] dark:bg-white text-white dark:text-[#1E293B]">
                 <Icon className="text-xl sm:text-3xl md:text-4xl " />
             </div>
-        </a>
+        </div>
     );
 };
 
