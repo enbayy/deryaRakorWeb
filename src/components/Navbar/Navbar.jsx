a325cf26f3425a3615dba9a09c7cab258906e5e3
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-03-20 23:00:00 +0300

    edited

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from "./ResponsiveMenu";
 import DarkMode from "./DarkMode";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/derya.png";
 
 export const MenuLinks = [
@@ -14,6 +14,7 @@ export const MenuLinks = [
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -33,13 +34,13 @@ const Navbar = () => {
                   to={link}
                   className={`relative px-4 py-2 inline-block text-lg font-bold font-sans transition-colors duration-300 
                     ${location.pathname === link
-                      ? "text-[#D22128] dark:text-[#D22128] font-bold font-sans border-[#D22128] dark:border-[#D22128]"
+                      ? "text-[#D22128] dark:text-[#D22128] font-bold font-sans border-b-2 border-[#D22128] dark:border-[#D22128]"
                       : "text-[#1E293B] dark:text-white hover:text-[#D22128] dark:hover:text-[#D22128] font-sans"
                     }
                     before:absolute before:bottom-0 before:left-0 before:w-full before:h-[3px] 
                     before:bg-[#D22128] dark:before:bg-[#D22128] before:scale-x-0 before:origin-right 
                     before:transition before:duration-300 before:ease-in-out 
-                    hover:before:scale-x-100 hover:before:origin-left font-sans
+                    hover:before:scale-x-100 hover:before:origin-left font-sans border-b
                   `}
                 >
                   {name}
@@ -49,14 +50,10 @@ const Navbar = () => {
           </ul>
         </nav>
         <div className="flex items-center gap-4">
-          <button
-            className="hidden lg:block text-white bg-[#D22128] hover:bg-black dark:hover:bg-red-400 transition-all duration-300 px-5 py-2 text-lg font-semibold rounded-xl font-sans"
-          >
+          <button className="hidden lg:block text-white bg-[#D22128] hover:bg-black dark:hover:bg-red-400 transition-all duration-300 px-5 py-2 text-lg font-semibold rounded-xl">
             TR
           </button>
-          <button
-            className="hidden lg:block text-white bg-[#D22128] hover:bg-black dark:hover:bg-red-400 transition-all duration-300 px-5 py-2 text-lg font-semibold font-sans rounded-xl"
-          >
+          <button className="hidden lg:block text-white bg-[#D22128] hover:bg-black dark:hover:bg-red-400 transition-all duration-300 px-5 py-2 text-lg font-semibold rounded-xl">
             EN
           </button>
           <DarkMode />
