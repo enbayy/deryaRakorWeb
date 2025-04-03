bcfe7771618b02f35d0c1b1780bb0743747a0921
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-04-03 13:11:17 +0300

    edited

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,46 +13,46 @@ const Footer = () => {
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-12 justify-center items-start">
                     <div className="flex flex-col items-center md:items-start">
                         <Link to="/" className="bg-white rounded-full">
-                            <img src={logo} alt="Derya Rakor Logo" className="w-48 h-auto" />
+                            <img src={logo} alt="Derya Rakor Logo" className="w-40 h-auto" />
                         </Link>
                         <div className="flex space-x-4 mt-8">
                             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                                <SiFacebook className="text-xl sm:text-2xl lg:text-3xl text-white dark:text-white hover:underline transition-colors" />
+                                <SiFacebook className="text-lg sm:text-xl lg:text-2xl text-white dark:text-white hover:underline transition-colors" />
                             </a>
                             <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                                <SiLinkedin className="text-xl sm:text-2xl lg:text-3xl text-white dark:text-white hover:underline transition-colors" />
+                                <SiLinkedin className="text-lg sm:text-xl lg:text-2xl text-white dark:text-white hover:underline transition-colors" />
                             </a>
                             <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                                <SiInstagram className="text-xl sm:text-2xl lg:text-3xl text-white dark:text-white hover:underline transition-colors" />
+                                <SiInstagram className="text-lg sm:text-xl lg:text-2xl text-white dark:text-white hover:underline transition-colors" />
                             </a>
                             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                                <SiTwitter className="text-xl sm:text-2xl lg:text-3xl text-white dark:text-white hover:underline transition-colors" />
+                                <SiTwitter className="text-lg sm:text-xl lg:text-2xl text-white dark:text-white hover:underline transition-colors" />
                             </a>
                         </div>
                     </div>
 
                     <div className="flex flex-col items-center md:items-start">
-                        <h4 className="text-2xl font-semibold text-white dark:text-white mb-4 font-sans">
+                        <h4 className="text-lg font-semibold text-white dark:text-white mb-4 font-sans">
                             {t("products")}
                         </h4>
                         <ul className="space-y-2">
                             <li>
-                                <a href="/urunlerimiz/hi̇droli̇k-si̇li̇ndi̇rler" className="text-lg text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
+                                <a href="/urunlerimiz/hi̇droli̇k-si̇li̇ndi̇rler" className="text-base text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
                                     {t("HİDROLİK SİLİNDİRLER")}
                                 </a>
                             </li>
                             <li>
-                                <a href="/urunlerimiz/hi̇droli̇k-si̇li̇ndi̇r-parcalari" className="text-lg text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
+                                <a href="/urunlerimiz/hi̇droli̇k-si̇li̇ndi̇r-parcalari" className="text-base text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
                                     {t("HİDROLİK SİLİNDİR PARÇALARI")}
                                 </a>
                             </li>
                             <li>
-                                <a href="/urunlerimiz/hi̇droli̇k-devre-borulari" className="text-lg text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
+                                <a href="/urunlerimiz/hi̇droli̇k-devre-borulari" className="text-base text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
                                     {t("HİDROLİK DEVRE BORULARI")}
                                 </a>
                             </li>
                             <li>
-                                <a href="/urunlerimiz/hi̇droli̇k-pnömati̇k-hortum-ve-basliklari" className="text-lg text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
+                                <a href="/urunlerimiz/hi̇droli̇k-pnömati̇k-hortum-ve-basliklari" className="text-base text-white dark:text-white font-sans hover:underline transition-colors font-semibold">
                                     {t("HİDROLİK-PNÖMATİK HORTUM VE BAŞLIKLARI")}
                                 </a>
                             </li>
@@ -60,14 +60,14 @@ const Footer = () => {
                     </div>
 
                     <div className="flex flex-col items-center md:items-start">
-                        <h4 className="text-2xl font-semibold text-white dark:text-white mb-4 font-sans">{t("contact")}</h4>
-                        <p className="text-lg text-white dark:text-white font-sans font-semibold">info@deryarakor.com</p>
-                        <p className="text-lg text-white dark:text-white font-sans font-semibold">(0332) 502 09 36</p>
+                        <h4 className="text-lg font-semibold text-white dark:text-white mb-4 font-sans">{t("contact")}</h4>
+                        <p className="text-base text-white dark:text-white font-sans font-semibold">info@deryarakor.com</p>
+                        <p className="text-base text-white dark:text-white font-sans font-semibold">(0332) 502 09 36</p>
                     </div>
 
                     <div className="flex flex-col items-center md:items-start text-center md:text-left">
-                        <h3 className="text-2xl font-semibold text-white dark:text-white mb-4 font-sans">{t("location")}</h3>
-                        <p className="text-lg leading-relaxed text-white dark:text-white font-sans font-semibold">
+                        <h3 className="text-lg font-semibold text-white dark:text-white mb-4 font-sans">{t("location")}</h3>
+                        <p className="text-base leading-relaxed text-white dark:text-white font-sans font-semibold">
                             Fevziçakmak, 10670. Sk. Bayır Özel Org. San. Bölg No:31 D:31A,<br />
                             42050 Karatay/Konya
                         </p>
@@ -77,7 +77,7 @@ const Footer = () => {
                 <hr className="border-white my-8 dark:border-[#F9FAFB]" />
 
                 <div className="text-center -mb-8">
-                    <p className="text-xl font-semibold text-white dark:text-white font-sans">
+                    <p className="text-lg font-semibold text-white dark:text-white font-sans">
                         {t('footer_info')}
                     </p>
                 </div>
